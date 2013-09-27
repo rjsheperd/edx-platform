8c07f4cd89c0003ccab835a2abec1aefa29b40b3
Alexander Kryklia <kryklia@gmail.com>
2013-09-27 16:59:17 +0300

    New feature for lti module.
    Add ability to display LTI module in a new
    window. Iframe will be still supported.
    
    Fix python test
    Update acceptance tests
    Fix in acceptance tests, they are stable now
    Fix Acceptance tests.
    Adding Jasmine tests for LTI.
    Fixing Jasmine tests. Adding the rest of Jasmine tests.

--- lms/djangoapps/courseware/tests/test_lti.py
+++ lms/djangoapps/courseware/tests/test_lti.py
@@ -76,5 +76,6 @@ class TestLTI(BaseTestXmodule):
             'element_class': self.item_module.location.category,
             'element_id': self.item_module.location.html_id(),
             'launch_url': 'http://www.example.com',  # default value
+            'open_in_a_new_page': True
         }
         self.assertDictEqual(generated_context, expected_context)
